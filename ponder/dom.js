0df069685abd394a6ee56b17db97d3e0e451296d
Sawyer Linginfelter <sawking3114@gmail.com>
2026-01-12 15:23:10 -0700

    moved things, did dom

---
+++ ponder/dom.js
@@ -0,0 +1,27 @@
+let title = document.querySelector('h1');
+console.log(title);
+
+title.textContent = 'Web Page Components';
+
+let topic_title = document.getElementById('topics')
+topic_title.style.color = 'red';
+
+let list = document.querySelector('.list');
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+para.classList.add('background');
+
+let image = document.querySelector('img');
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.querySelector('#html').style.color = 'red';
+})
+
+
+const newPara = document.createElement('p');
+newPara.innetText = 'Added with Javascript';
+document.body.appendChild(newPara);
